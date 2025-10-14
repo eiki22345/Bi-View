449dda13656da78e95e1914ed17873af39a751b1
eiki <eiki22345@gmial.com>
2025-10-14 16:56:41 +0900

    add_file

---
+++ laravel-samuraimart-kadai-add-sort/resources/views/layouts/app.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <script src="https://kit.fontawesome.com/a251afe25c.js" crossorigin="anonymous"></script>
+
+    <link href="{{ asset('css/samuraimart.css')}}" rel="stylesheet">
+</head>
+
+<body>
+    <div id="app">
+        @component('components.header')
+        @endcomponent
+
+        <main class="py-4 mb-5">
+            @yield('content')
+        </main>
+
+        @component('components.footer')
+        @endcomponent
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous" differ></script>
+</body>
+
+</html>
